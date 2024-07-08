f4b096196b3ae839be62639b5cd4882b442f6f9d
sineptic <sineptic0@gmail.com>
2024-07-08 22:23:47 +0300

    move generic parametr to associated type

--- src/task/level.rs
+++ src/task/level.rs
@@ -1,8 +1,9 @@
 use serde::{de::DeserializeOwned, Serialize};
 use std::time::Duration;
 
-pub trait TaskLevel<C>: Default + Serialize + DeserializeOwned {
-    fn success(&mut self, context: C);
-    fn failure(&mut self, context: C);
+pub trait TaskLevel: Default + Serialize + DeserializeOwned {
+    type Context;
+    fn success(&mut self, context: Self::Context);
+    fn failure(&mut self, context: Self::Context);
     fn until_next_repetition(&self) -> Duration;
 }
