616dcdb816caa81d34bb402b584bffae23211026
sineptic <sineptic0@gmail.com>
2024-07-08 21:16:54 +0300

    rename method

--- src/task/level.rs
+++ src/task/level.rs
@@ -4,5 +4,5 @@ use std::time::Duration;
 pub trait TaskLevel<C>: Default + Serialize + DeserializeOwned {
     fn success(&mut self, context: C);
     fn failure(&mut self, context: C);
-    fn duration(&self) -> Duration;
+    fn until_next_repetition(&self) -> Duration;
 }
