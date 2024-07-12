f78e9a67f29114436c58007f7fa60480ecd49b2a
sineptic <sineptic0@gmail.com>
2024-07-12 19:42:24 +0300

    change TaskLevel trait

--- src/task/level.rs
+++ src/task/level.rs
@@ -3,7 +3,6 @@ use std::time::Duration;
 
 pub trait TaskLevel: Default + Serialize + DeserializeOwned {
     type Context;
-    fn success(&mut self, context: Self::Context);
-    fn failure(&mut self, context: Self::Context);
+    fn update(&mut self, context: Self::Context);
     fn until_next_repetition(&self) -> Duration;
 }
