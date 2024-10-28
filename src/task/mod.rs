482f7b31773bebbab6417bb23fc4c17962e99efa
sineptic <sineptic0@gmail.com>
2024-10-28 14:35:40 +0300

    fix!: remove Option

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -7,7 +7,7 @@ pub trait Task<'a>: Serialize + Deserialize<'a> {
     type SharedState: SharedState<'a>;
 
     /// blocks must contain interactive elements
-    fn new(input: s_text_input_f::BlocksWithAnswer) -> Option<Self>;
+    fn new(input: s_text_input_f::BlocksWithAnswer) -> Self;
     fn get_blocks(&self) -> s_text_input_f::BlocksWithAnswer;
 
     fn next_repetition(
